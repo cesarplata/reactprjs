8670e18c588dd39f017eb85b221e9771700936c8
Cesar Plata <cesar.plata@premize.com>
2023-02-09 16:45:27 -0500

    second commit

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -28,7 +28,7 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
-        <label>Course Goal</label>
+        <label>Course Goal example</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
