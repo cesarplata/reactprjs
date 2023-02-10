06573f42f57ac8f63bedf07c784a116e71138889
Cesar Plata <cesar.plata@premize.com>
2023-02-10 14:29:48 -0500

    copy on clipboard

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Message from '../../Messages/Message';
 
 import './CourseGoalItem.css';
 
 const CourseGoalItem = props => {
-  // const [deleteText, setDeleteText] = useState('');
+  const [deleteText, setDeleteText] = useState(false);
 
   const deleteHandler = () => {
-    // setDeleteText('(Deleted!)');
-    props.onDelete(props.id);
+    setDeleteText(true);
+    console.info(props.children);
+    navigator.clipboard.writeText(props.children);
+    //props.onDelete(props.id);
   };
 
   return (
-    <li className="goal-item" onClick={deleteHandler}>
-      {props.children}
-    </li>
+    <section>
+      <li className="goal-item" onClick={deleteHandler}>
+        {props.children}
+      </li>
+      {deleteText && (<Message >copied</Message>)}
+    </section>
   );
 };
 
